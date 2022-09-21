cef31d0dafe819b79f1d83f1c7a395e27a9fe356
wangmu <wangmu8889@gmail.com>
2022-09-21 13:58:22 +0800

    update 提取词法单元

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,5 +1,7 @@
 package lexer
 
+import "github.com/iWangMu/Interpreter-Go/token"
+
 type Lexer struct {
 	// 输入字符串
 	input string
@@ -11,11 +13,41 @@ type Lexer struct {
 	ch byte
 }
 
+// 初始化
 func New(input string) *Lexer {
 	l := &Lexer{input: input}
 	return l
 }
 
+// 提取词法单元
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+	switch l.ch {
+	case '=':
+		tok = newToken(token.ASSIGN, l.ch)
+	case '+':
+		tok = newToken(token.PLUS, l.ch)
+	case ',':
+		tok = newToken(token.COMMA, l.ch)
+	case ';':
+		tok = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		tok = newToken(token.LPAREN, l.ch)
+	case ')':
+		tok = newToken(token.RPAREN, l.ch)
+	case '{':
+		tok = newToken(token.LBRACE, l.ch)
+	case '}':
+		tok = newToken(token.RBRACE, l.ch)
+	case 0:
+		tok.Type = token.EOF
+		tok.Literal = ""
+	}
+	l.readChar()
+	return tok
+}
+
+// 读取input的下一个字符，只支持ASCII字符集
 func (l *Lexer) readChar() {
 	if l.readPosition >= len(l.input) {
 		// ASCII编码表示NUL字符，表示尚未读取任何内容或文件结尾
@@ -25,4 +57,9 @@ func (l *Lexer) readChar() {
 	}
 	l.position = l.readPosition
 	l.readPosition += 1
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+
+	return token.Token{Type: tokenType, Literal: string(ch)}
 }
